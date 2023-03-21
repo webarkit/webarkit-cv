6c9bdaa6d7ea5d1fb3fbb826aedf3ceec6aa15a3
Walter Perdan <github@kalwaltart.it>
2023-03-21 23:07:59 +0100

    build as ES6 module with --build_flags instead

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # remember to 'export EMSCRIPTEN="/path/to/emsdk/upstream/emscripten"' before running this script !
 
-python3 ./opencv/platforms/js/build_js.py opencv_js --config="./opencv.webarkit_config.py" --build_wasm --cmake_option="-DBUILD_opencv_dnn=OFF"  --cmake_option="-DBUILD_opencv_objdetect=OFF" --cmake_option="-DBUILD_opencv_photo=OFF"
+python3 ./opencv/platforms/js/build_js.py opencv_js --config="./opencv.webarkit_config.py" --build_wasm --cmake_option="-DBUILD_opencv_dnn=OFF"  --cmake_option="-DBUILD_opencv_objdetect=OFF" --cmake_option="-DBUILD_opencv_photo=OFF" --build_flags="-s EXPORT_ES6=1 -s USE_ES6_IMPORT_META=0"
 
 cp -r opencv_js/bin/opencv_js.js build
 cp -r opencv_js/bin/opencv.js build
