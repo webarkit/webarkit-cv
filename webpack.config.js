7259559f1d52934bc11cc159d293252bc52eacdb
Walter Perdan <github@kalwaltart.it>
2023-03-22 23:45:43 +0100

    adding typescript support

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
-    path: path.resolve('dist'),
+    path: path.resolve(__dirname, "dist"),
     filename: 'webarkitCV.js',
     library: 'WebARKitCV',
     libraryTarget: 'umd',
@@ -11,28 +11,23 @@ module.exports = {
     //libraryExport: 'default',
     // @see: https://github.com/webpack/webpack/issues/6522
     globalObject: 'typeof self !== \'undefined\' ? self : this',
-    //globalObject: 'this',
   },
   module: {
     rules: [
       {
-        test: /\.js?$/,
+        test: /\.js$/,
+        use: "babel-loader",
         exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: [
-              // @see https://github.com/babel/babel/issues/9849
-              ['@babel/transform-runtime']
-            ]
-          }
-        }]
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.js'],
+    extensions: [".tsx", ".ts", ".js"],
     modules: ['node_modules'],
     // @see https://stackoverflow.com/questions/59487224/webpack-throws-error-with-emscripten-cant-resolve-fs
     fallback: {
