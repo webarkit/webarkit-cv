d48133c0cce2db2500f758e9420f48dbb8e6237d
Walter Perdan <github@kalwaltart.it>
2023-03-24 16:25:12 +0100

    avoid importing opencv inside WebARKitCV

--- dist/src/interfaces/WebARKitCVBuilder.js
+++ dist/src/interfaces/WebARKitCVBuilder.js
@@ -4,14 +4,12 @@ export class WebARKitBase {
     trackable;
     trackables;
     isLoaded;
-    opencv;
-    constructor(width, height, trackable, trackables, isLoaded, opencv) {
+    constructor(width, height, trackable, trackables, isLoaded) {
         this.width = width;
         this.height = height;
         this.trackable = trackable;
         this.trackables = trackables;
         this.isLoaded = isLoaded;
-        this.opencv = opencv;
     }
 }
 //# sourceMappingURL=WebARKitCVBuilder.js.map
