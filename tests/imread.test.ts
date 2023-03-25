97a1ce5f5a815ce4909a1838b848850ed1cb3edf
Walter Perdan <github@kalwaltart.it>
2023-03-25 15:27:49 +0100

    formatting tests with prettier

--- tests/imread.test.ts
+++ tests/imread.test.ts
@@ -1,26 +1,28 @@
 /**
  * @jest-environment jsdom
  */
-import {describe, expect, jest, test} from '@jest/globals';
-import { imread } from '../src/io/imgFunctions';
+import { describe, expect, jest, test } from "@jest/globals";
+import { imread } from "../src/io/imgFunctions";
 
-jest.mock('../src/io/imgFunctions')
+jest.mock("../src/io/imgFunctions");
 
-describe('A simple test for the imread function', () => {
-    test('Test if the imread function is well defined', () => {
-        document.body.innerHTML = '<img src="./pinball.jpg" id="pinball" /> <canvas></canvas>';
-        imread('pinball')
-        expect(imread).toBeDefined();
-    })
-})
+describe("A simple test for the imread function", () => {
+  test("Test if the imread function is well defined", () => {
+    document.body.innerHTML =
+      '<img src="./pinball.jpg" id="pinball" /> <canvas></canvas>';
+    imread("pinball");
+    expect(imread).toBeDefined();
+  });
+});
 
-var i = imread('pinball');
+var i = imread("pinball");
 
-describe('Another test for the imread function', () => {
-  test('Test if the imread function return imageData', () => {
-      document.body.innerHTML = '<img src="../examples/pinball.jpg" id="pinball" /> <canvas></canvas>';
-      var imreadMocked = imread as jest.Mocked<typeof imread>; 
-      
-      imreadMocked.mockReturnValue(i);
-  })
-})
+describe("Another test for the imread function", () => {
+  test("Test if the imread function return imageData", () => {
+    document.body.innerHTML =
+      '<img src="../examples/pinball.jpg" id="pinball" /> <canvas></canvas>';
+    var imreadMocked = imread as jest.Mocked<typeof imread>;
+
+    imreadMocked.mockReturnValue(i);
+  });
+});
