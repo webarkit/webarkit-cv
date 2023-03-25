57829f069a55140ee33c258e9765cd4be94b4600
Walter Perdan <github@kalwaltart.it>
2023-03-25 15:27:49 +0100

    formatting tests with prettier

--- tests/Trackable.test.ts
+++ tests/Trackable.test.ts
@@ -1,21 +1,20 @@
-import {describe, expect, jest, test} from '@jest/globals';
+import { describe, expect, jest, test } from "@jest/globals";
 import { ITrackable, Trackable } from "../src/interfaces/Trackables";
 import { v4 as uuidv4 } from "uuid";
 
-describe('A simple test for the Trackable class', () => {
-    test('Test if the Trackable class is well defined', () => {
-        expect(Trackable).toBeDefined();
-    })
-})
+describe("A simple test for the Trackable class", () => {
+  test("Test if the Trackable class is well defined", () => {
+    expect(Trackable).toBeDefined();
+  });
+});
 
-describe('Another test for the Trackable class', () => {
-    test('Test if the Trackable class return a new Trackable', () => {
-        const uuid: string = uuidv4();
-        const t: ITrackable = new Trackable("pinball.jpg", "pinball", uuid);
-        
-        expect(t.name).toBe("pinball");
-        expect(t.url).toBe("pinball.jpg");
-        expect(t.uuid).toBe(uuid);
-    })
-})
+describe("Another test for the Trackable class", () => {
+  test("Test if the Trackable class return a new Trackable", () => {
+    const uuid: string = uuidv4();
+    const t: ITrackable = new Trackable("pinball.jpg", "pinball", uuid);
 
+    expect(t.name).toBe("pinball");
+    expect(t.url).toBe("pinball.jpg");
+    expect(t.uuid).toBe(uuid);
+  });
+});
