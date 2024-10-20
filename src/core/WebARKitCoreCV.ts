149b45f6d599199662497089d424ae4790a6f42d
Walter Perdan <github@kalwaltart.it>
2024-10-20 23:33:47 +0200

    valid homography is correct

--- src/core/WebARKitCoreCV.ts
+++ src/core/WebARKitCoreCV.ts
@@ -98,12 +98,15 @@ export class WebARKitCoreCV {
   track(msg: any) {
     var result;
     const keyFrameImageData = msg.imagedata;
+    console.log(keyFrameImageData)
 
     let src = new this.cv.Mat(msg.vHeight, msg.vWidth, this.cv.CV_8UC4);
+    let gray = new this.cv.Mat(msg.vHeight, msg.vWidth, this.cv.CV_8UC1);
 
     src.data.set(keyFrameImageData);
 
-    this.cv.cvtColor(src, src, this.cv.COLOR_RGBA2GRAY, 0);
+    this.cv.cvtColor(src, gray, this.cv.COLOR_RGBA2GRAY, 0);
+    console.log(gray)
 
     let ksize = new this.cv.Size(this.BlurSize, this.BlurSize);
     let anchor = new this.cv.Point(-1, -1);
@@ -118,7 +121,7 @@ export class WebARKitCoreCV {
     var noArray = new this.cv.Mat();
 
     orb.detectAndCompute(
-      src,
+      gray,
       noArray,
       frame_keypoints_vector,
       frame_descriptors,
@@ -150,15 +153,21 @@ export class WebARKitCoreCV {
 
       if (point.distance < 0.7 * point2.distance) {
         var frame_point = frame_keypoints_vector.get(point.queryIdx).pt;
-        frame_keypoints.push(frame_point);
+        console.log(frame_point.x)
+        //frame_keypoints.push(frame_point);
+        frame_keypoints.push(frame_point.x);
+        frame_keypoints.push(frame_point.y);
 
         var template_point = this.template_keypoints_vector.get(
           point.trainIdx,
         ).pt;
 
-        template_keypoints.push(template_point);
+        //template_keypoints.push(template_point);
+        template_keypoints.push(template_point.x);
+        template_keypoints.push(template_point.y);
       }
     }
+    console.log(template_keypoints)
 
     var frameMat = new this.cv.Mat(frame_keypoints.length, 1, this.cv.CV_32FC2);
     var templateMat = new this.cv.Mat(
@@ -167,29 +176,36 @@ export class WebARKitCoreCV {
       this.cv.CV_32FC2,
     );
 
-    for (let i = 0; i < template_keypoints.length; i++) {
+    frameMat.data32F.set(frame_keypoints)
+    templateMat.data32F.set(template_keypoints)
+
+    /*for (let i = 0; i < template_keypoints.length; i++) {
       frameMat.data32F[i * 2] = frame_keypoints[i].x;
       frameMat.data32F[i * 2 + 1] = frame_keypoints[i].y;
 
       templateMat.data32F[i * 2] = template_keypoints[i].x;
       templateMat.data32F[i * 2 + 1] = template_keypoints[i].y;
-    }
+    }*/
 
     if (template_keypoints.length >= this.ValidPointTotal) {
       var homography = this.cv.findHomography(
-        templateMat,
-        frameMat,
-        this.cv.RANSAC,
+          frameMat,
+          templateMat,
+          this.cv.RANSAC,
       );
+      console.log("homograpy: ",homography);
       var valid;
 
       valid = this.homographyValid(homography);
+      console.log(valid)
 
-      if (this.homographyValid(homography) === true) {
-        var out = this.fill_output(homography, valid);
-        console.log("output from", out);
+      if (this.homographyValid(homography) == true) {
+          console.log('true')
+          var out = this.fill_output(homography, valid);
+          console.log("output from", out);
       }
-      this.homography_transform = homography.data64F;
+      //this.homography_transform = homography.data64F;
+      this.homography_transform = out.slice(0,9)
     } else {
       this.homography_transform = null;
     }
@@ -216,8 +232,10 @@ export class WebARKitCoreCV {
   }
 
   homographyValid(H: any) {
+    console.log(H.floatAt(0,0))
     const det =
       H.doubleAt(0, 0) * H.doubleAt(1, 1) - H.doubleAt(1, 0) * H.doubleAt(0, 1);
+      //H.floatAt(0, 0) * H.floatAt(1, 1) - H.floatAt(1, 0) * H.floatAt(0, 1);
 
     return 1 / this.N < Math.abs(det) && Math.abs(det) < this.N;
   }
