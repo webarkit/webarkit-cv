d1c0a6cc04a5a2bacf631e31a39b8e1dbcf6d727
Walter Perdan <github@kalwaltart.it>
2025-10-27 16:43:02 +0100

    style: standardize quotation marks and formatting in test files and configuration

--- jest.esm.config.cjs
+++ jest.esm.config.cjs
@@ -1,19 +1,19 @@
 module.exports = {
-  roots: ['<rootDir>/tests-esm'],
+  roots: ["<rootDir>/tests-esm"],
   transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
+    "^.+\\.tsx?$": [
+      "ts-jest",
       {
-        tsconfig: '<rootDir>/tsconfig.jest.esm.json',
-        useESM: true
-      }
-    ]
+        tsconfig: "<rootDir>/tsconfig.jest.esm.json",
+        useESM: true,
+      },
+    ],
   },
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   moduleNameMapper: {
-    './Worker': '<rootDir>/__mocks__/workerMock.js'
+    "./Worker": "<rootDir>/__mocks__/workerMock.js",
   },
-  testEnvironment: 'node'
+  testEnvironment: "node",
 };
